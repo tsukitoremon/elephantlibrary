9d4cfc086079a250c65dc066dc333b72f7341fd5
tsukitoremon <33226313+tsukitoremon@users.noreply.github.com>
2023-10-27 23:24:22 +0900

    テーブルの設定（暫定）

--- db/migrate/20231026132320_create_books.rb
+++ db/migrate/20231026132320_create_books.rb
@@ -1,7 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-
+      t.string :bookname            null: false
+      t.string :author              null: false
+      t.string :text
+      t.string :year
       t.timestamps
     end
   end
