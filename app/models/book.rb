22942f71b7265903f65db4420a3e66c2e631e97e
tsukitoremon <33226313+tsukitoremon@users.noreply.github.com>
2023-11-01 20:27:36 +0900

    activehash導入

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+    #空の投稿を保存できないようにする
+    validates :bookname, :author, presence: true
+
+    #ジャンルの選択が「---」の時は保存できないようにする
+    validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- db/migrate/20231026132320_create_books.rb
+++ db/migrate/20231026132320_create_books.rb
@@ -1,10 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :bookname            null: false
-      t.string :author              null: false
-      t.string :text
-      t.string :year
+      t.string  :bookname          , null: false
+      t.string  :author            , null: false
+      t.text    :text
+      t.integer :year
+      t.integer :genre_id          , null: false
       t.timestamps
     end
   end
