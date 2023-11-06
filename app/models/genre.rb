f011c1f64f172a12cee9258b17309e7a15adbba9
tsukitoremon <33226313+tsukitoremon@users.noreply.github.com>
2023-11-06 22:06:55 +0900

    ジャンル修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,15 +1,16 @@
 class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 2, name: '情報・IT' },
+    { id: 3, name: '哲学・心理' },
+    { id: 4, name: '歴史・地理' },
+    { id: 5, name: '社会科学' },
+    { id: 6, name: '自然科学' },
+    { id: 7, name: '家政学・技術' },
+    { id: 8, name: '産業' },
+    { id: 9, name: '芸術・マンガ' },
+    { id: 10, name: '言語学' },
+    { id: 11, name: '文学' }
   ]
 
   include ActiveHash::Associations
