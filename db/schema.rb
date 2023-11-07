5339c7255784670f5a22b8c374655aa85f4f6dbe
tsukitoremon <33226313+tsukitoremon@users.noreply.github.com>
2023-11-07 20:59:35 +0900

    booksコントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_132320) do
+  create_table "books", charset: "utf8", force: :cascade do |t|
+    t.string "bookname", null: false
+    t.string "author", null: false
+    t.text "text"
+    t.integer "year"
+    t.integer "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
