97a1df6f2aefda9d8579f3cde0549c91f74a0c94
tsukitoremon <33226313+tsukitoremon@users.noreply.github.com>
2023-11-14 20:21:09 +0900

    viewの基礎設置

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,4 @@
-class BooksController < ApplicationController]
+class BooksController < ApplicationController
   def index
     @books = Book.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :books, only: :index
+  root to: "books#index"
 end
